7abac1a2b991984de64793f256441d23fdc9bea4
RATOR-codes <164455832+RATOR-codes@users.noreply.github.com>
2025-02-21 12:18:09 +0800

    Create ifos.py

---
+++ ifos.py
@@ -0,0 +1,11 @@
+if os.path.exists("/tmp/user_file"):
+    os.remove("/tmp/user_file")
+
+if .path.exists("/tmp/user_file"):\n .remove("/tmp/user_file")
+if os.path.("/tmp/user_file"):\n os.remove("/tmp/user_file")
+if os..exists("/tmp/user_file"):\n os.remove("/tmp/user_file")
+if os.path.exists("/tmp/user_file"):\n os.("/tmp/user_file")
+if os..exists("/tmp/user_file"):\n os.("/tmp/user_file")
+if os.path.("/tmp/user_file"):\n os.("/tmp/user_file")
+if .path.("/tmp/user_file"):\n .remove("/tmp/user_file")
+if ..exists("/tmp/user_file"):\n .remove("/tmp/user_file")
